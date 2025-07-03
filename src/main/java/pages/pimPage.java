51efb9eeaf92a7e285ef1eb97e13c1c6037137dc
prafpawar11 <prafulp1030@gmail.com>
2025-07-03 08:42:01 +0530

    Add Employee in PIM Page by Aditi

--- src/main/java/pages/pimPage.java
+++ src/main/java/pages/pimPage.java
@@ -2,9 +2,15 @@ package pages;
 
 public class pimPage {
 
-	
-	public void clickOnPimLink()
-	{
+	public void clickOnPimLink() {
 		System.out.println("Clicked on PIM link");
+
 	}
+
+	public void addEmployee(String fname, String lname) {
+		System.out.println(fname);
+		System.out.println(lname);
+		System.out.println("Employee added");
+	}
+
 }
